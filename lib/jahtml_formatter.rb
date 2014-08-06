674e70eb51251182f0047a82becc2656af87f992
John Axel Eriksson <john@insane.se>
2014-08-06 02:19:44 +0200

    RSpec 3 compatible

--- lib/jahtml_formatter.rb
+++ lib/jahtml_formatter.rb
@@ -5,6 +5,14 @@ require 'iso_example_group_methods'
 require 'iso_example_methods'
 require 'rspec/core'
 
+
+RSPEC_3_COMPAT = if defined?(RSpec::Core::Version)
+                   rspec_version = RSpec::Core::Version::STRING.split('.').map { |e| e.to_i }
+                   rspec_version[0]>=3 || (rspec_version[0]>=2 && rspec_version[1]>=99)
+                 else
+                   false
+                 end
+
 class JahtmlFormatter < RSpec::Core::Formatters::HtmlFormatter
   def initialize(output)
     super(output)
@@ -12,9 +20,18 @@ class JahtmlFormatter < RSpec::Core::Formatters::HtmlFormatter
   end
 end
 
-RSpec.configure do |config|
-  config.include IsoExampleGroupMethods
-  config.before do
-    example.extend IsoExampleMethods
+if RSPEC_3_COMPAT
+  RSpec.configure do |config|
+    config.include IsoExampleGroupMethods
+    config.before do |example|
+      example.extend IsoExampleMethods
+    end
+  end
+else
+  RSpec.configure do |config|
+    config.include IsoExampleGroupMethods
+    config.before do
+      example.extend IsoExampleMethods
+    end
   end
 end
