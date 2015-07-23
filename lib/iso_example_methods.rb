6e936ae35d9f2cca8cd98586a87123f8fd40eea2
yihyunyi <yihyunyi@bluedigm.com>
2015-07-23 16:28:42 +0900

    Update iso_example_methods.rb

--- lib/iso_example_methods.rb
+++ lib/iso_example_methods.rb
@@ -1,12 +1,12 @@
 # coding: UTF-8
 module IsoExampleMethods
   def description
-    if metadata.key?(:iso_id)
+    if metadata.key?(:key)
       ## if the description is empty... we don't do anything
       ## I guess it can be empty if an it is defined like so:
       ## it { should have(5).items } - what then?
-      unless metadata[:description].to_s.empty? || metadata[:description].include?(@options[:iso_id])
-        metadata[:description] = "#{metadata[:description]}, iso: #{@options[:iso_id]}"
+      unless metadata[:description].to_s.empty? || metadata[:description].include?(metadata[:key])
+        metadata[:description] = "#{metadata[:description]}, iso: #{metadata[:key]}"
       end
     end
     super
