c5f427d3a67f64ff855784d18a8973389cd4f561
yihyunyi <yihyunyi@bluedigm.com>
2015-07-23 17:10:48 +0900

    Update iso_example_methods.rb

--- lib/iso_example_methods.rb
+++ lib/iso_example_methods.rb
@@ -5,8 +5,8 @@ module IsoExampleMethods
     ## I guess it can be empty if an it is defined like so:
     ## it { should have(5).items } - what then?
     unless metadata[:description].to_s.empty?
-    #metadata[:description] = "#{metadata[:description]}"
-      
+     metadata[:description] = "#{metadata[:description]}"
+    end  
     super
   end
 end
