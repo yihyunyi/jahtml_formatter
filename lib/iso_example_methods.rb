3ac8e3097a21ce81f9f7bc85bcd754f1246dc02b
yihyunyi <yihyunyi@bluedigm.com>
2015-07-23 17:09:09 +0900

    Update iso_example_methods.rb

--- lib/iso_example_methods.rb
+++ lib/iso_example_methods.rb
@@ -1,16 +1,12 @@
 # coding: UTF-8
 module IsoExampleMethods
   def description
-    puts "[hy] #{metadata}"
-    
-    if metadata.key?(:iso_id)
-      ## if the description is empty... we don't do anything
-      ## I guess it can be empty if an it is defined like so:
-      ## it { should have(5).items } - what then?
-      unless metadata[:description].to_s.empty? || metadata[:description].include?(metadata[:iso_id])
-        metadata[:description] = "#{metadata[:description]}, iso: #{metadata[:iso_id]}"
-      end
-    end
+    ## if the description is empty... we don't do anything
+    ## I guess it can be empty if an it is defined like so:
+    ## it { should have(5).items } - what then?
+    unless metadata[:description].to_s.empty?
+    #metadata[:description] = "#{metadata[:description]}"
+      
     super
   end
 end
